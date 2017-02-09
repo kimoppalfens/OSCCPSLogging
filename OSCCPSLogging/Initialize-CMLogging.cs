10afefbf16c49ff7a878b28813ab99af1703ebe5
Kim Oppalfens <kim.oppalfens@hotmail.com>
2017-02-09 12:34:16 +0100

    Add support for overriding filename & loglevel

--- OSCCPSLogging/Initialize-CMLogging.cs
+++ OSCCPSLogging/Initialize-CMLogging.cs
@@ -9,60 +9,100 @@ using System.IO;
 using System.Reflection;
 
 
+//Todo override console level
 namespace OSCCPSLogging
 {
 //Todo add properties
+//Todo Paramater validation
     [Cmdlet(VerbsData.Initialize,"CMLogging")]
     public class OSCCPSLogging : PSCmdlet
      {
 
-        //log4net.ILog logger;
+        private string _fileLogLevel="Info";
+        [Parameter(
+        Mandatory = false,
+        ValueFromPipeline = true,
+        ValueFromPipelineByPropertyName = true,
+        Position = 2,
+        HelpMessage = "Specify a loglevel, supported values are off, info, warn, error, debug.")]
+
+        public string fileLogLevel
+        {
+            get { return _fileLogLevel; }
+            set { _fileLogLevel = value; }
+        }
+
+        private string _logFileName;
+        [Parameter(
+        Mandatory = false,
+        ValueFromPipeline = true,
+        ValueFromPipelineByPropertyName = true,
+        Position = 0,
+        HelpMessage = "Specify the full path including filename to a cmtrace formatted logging file.")]
+
+        public string LogFileName
+        {
+            get { return _logFileName; }
+            set { _logFileName = value; }
+        }
+
 
         private string _logConfigFile = string.Concat(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"\logging.config");
-
         [Parameter(
             Mandatory = false,
             ValueFromPipeline = true,
             ValueFromPipelineByPropertyName = true,
-            Position = 0,
+            Position = 1,
             HelpMessage = "Specify the full path to a Log4Net formatted logging configuration file.")]
+        
         public string LogConfigFile
         {
             get { return _logConfigFile; }
             set { _logConfigFile = value;}
         }
 
-
-       protected override void BeginProcessing()
-        {
-            base.BeginProcessing();
-            Log4Net_CMTrace.UTCOffsetPatternConverter utc = new Log4Net_CMTrace.UTCOffsetPatternConverter();
-            
-        }
+       //protected override void BeginProcessing()
+       // {
+       //     base.BeginProcessing();
+       // }
 
         protected override void ProcessRecord()
         {
-            //base.ProcessRecord();
-            //log4net.LogManager logManager;
-            
-
-
-            Console.WriteLine(_logConfigFile);
+            //Console.WriteLine(_logConfigFile);
             log4net.ILog logger = log4net.LogManager.GetLogger("Powershell");
+            log4net.Appender.IAppender[] appenders = log4net.LogManager.GetRepository().GetAppenders();
 
             System.IO.FileInfo logFileInfo = new System.IO.FileInfo(_logConfigFile);
             log4net.Config.XmlConfigurator.ConfigureAndWatch(logFileInfo);
             WriteObject(logger);
 
-            log4net.Appender.RollingFileAppender  fileAppender = (log4net.Appender.RollingFileAppender)log4net.LogManager.GetRepository().GetAppenders()[1];
-            fileAppender.ActivateOptions();
-            //string logFileName = 
+            foreach (log4net.Appender.IAppender logAppender in appenders)
+            {
+                if (logAppender.GetType() == typeof(log4net.Appender.RollingFileAppender))
+                {
+                    log4net.Appender.FileAppender fileAppender = (log4net.Appender.RollingFileAppender)logAppender;
+                    //switch 
+                    
+                if (MyInvocation.BoundParameters.ContainsKey("LogFileName"))
+                {
+                    fileAppender.File = LogFileName;
+                    
+                }
+                if (MyInvocation.BoundParameters.ContainsKey("FileLogLevel"))
+                {
+                    switch (fileLogLevel.ToLower())
+                    {
+                            case "off":     fileAppender.Threshold = log4net.Core.Level.Off; break;
+                            case "info":    fileAppender.Threshold = log4net.Core.Level.Info; break;
+                            case "warn":    fileAppender.Threshold = log4net.Core.Level.Warn; break;
+                            case "error":   fileAppender.Threshold = log4net.Core.Level.Error; break;
+                            case "debug":   fileAppender.Threshold = log4net.Core.Level.Debug; break;
+                            default:        fileAppender.Threshold = log4net.Core.Level.Info; break;
+                    }
+                }
+                fileAppender.ActivateOptions();
+                }
+            }
         }
-
-        //protected override void EndProcessing()
-        //{
-        //    base.EndProcessing();
-        //}
-
     }
 }
